5628fcf8e7d7a836f0f46465aa7e65f8ae1773c7
Christiano Braga <cbraga@ic.uff.br>
2025-05-05 15:01:38 -0300

    Changed grader/test-cmd.sh

--- grader/test-cmd.sh
+++ grader/test-cmd.sh
@@ -1,2 +1,11 @@
-OUT=$(java -cp "chocopy-ref.jar:target/assignment.jar" chocopy.ChocoPy --pass=s --test --dir src/test/data/pa1/sample/expr_plus.py | grep "1 passed")
-echo $?
+#!/usr/bin/bash
+
+OUT=$(java -cp "chocopy-ref.jar:target/assignment.jar" chocopy.ChocoPy --pass=s --test --dir $1 | grep "1 passed")
+echo $OUT
+
+if [[ "$OUT" == "" ]]; then
+    exit 1
+else
+    exit 0
+fi
+       
